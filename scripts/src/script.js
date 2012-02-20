2f9325eccee327f96fad6c847d825fb716ebfd16
Kirk Daniels <kdaniels@unca.edu>
2012-02-20 17:36:58 -0500

    added input box

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,31 +1,27 @@
 function main() {
+  var term="unca";
+
+  $("#searchbtn").click(function(){
+    term = $("#term").val();
+  });
 
   var s = new Spotter("twitter.search",
-                      {q:"wilkos", lang:"en", period:120},
+                      {q:term, lang:"en", period:120},
                       {buffer:true,bufferTimeout:1000});
   var count = 0;
   var removeCount = 0;
   var color = 0;
-  var maury = 0;
-  var searchTerms = [
-                    ["Husband", 0],
-                    ["Wife", 0],
-                    ]"Boyfriend", 0],
-                    ["Girlfriend", 0],
-                    ["Son", 0],
-                    ["Daughter", 0],
-                    ["Cousin", 0]
-                    ];
 
   s.register(function(tweets) {
     //$("#results").replaceWith("<div id='results' class='span2'></div>");
     //$("#results").replaceWith("<div id='results'>Maury: "+maury+"</div>");
+    $("#results").replaceWith("<div id='results' class='span16'>"+term+"</div>");
 
     var txt = tweets.text;
     var img = tweets.profile_image_url;
 
-    searchTerms.each(function(){
-    });
+    //searchTerms.each(function(){
+    //});
 
     if(color%2===0) {
       var style = "tweet0";
@@ -43,9 +39,9 @@ function main() {
     }
     count++;
     color++;
-    if (txt.match(/maury/i)) {
-      maury++;
-    }
+
+      //if (txt.match(/value/i)){
+      //}
   });
 
   s.start();
