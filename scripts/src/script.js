6eea49824c1b01902a31e47259bf2191f6ecbb65
Kirk Daniels <kdaniels@unca.edu>
2012-02-20 18:00:36 -0500

    button starts spotter

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,50 +1,48 @@
 function main() {
-  var term="unca";
 
   $("#searchbtn").click(function(){
     term = $("#term").val();
-  });
 
-  var s = new Spotter("twitter.search",
-                      {q:term, lang:"en", period:120},
-                      {buffer:true,bufferTimeout:1000});
-  var count = 0;
-  var removeCount = 0;
-  var color = 0;
+    var s = new Spotter("twitter.search",
+                        {q:term, lang:"en", period:120},
+                        {buffer:true,bufferTimeout:1000});
 
-  s.register(function(tweets) {
-    //$("#results").replaceWith("<div id='results' class='span2'></div>");
-    //$("#results").replaceWith("<div id='results'>Maury: "+maury+"</div>");
-    $("#results").replaceWith("<div id='results' class='span16'>"+term+"</div>");
+    var count = 0;
+    var removeCount = 0;
+    var color = 0;
+    var termcount = 0;
 
-    var txt = tweets.text;
-    var img = tweets.profile_image_url;
+    s.register(function(tweets) {
 
-    //searchTerms.each(function(){
-    //});
+      $("#results").replaceWith("<div id='results' class='span16'>"+termcount+" tweets about "+term+" mention UNCA.</div>");
 
-    if(color%2===0) {
-      var style = "tweet0";
-    } else {
-      var style = "tweet1";
-    }
-    var tweet = $("<p id='"+count+"' class='"+style+"'><img src='"+img+"'/>"+txt+"</p>");
-    tweet.hide();
-    $("#tweets").prepend(tweet);
-    tweet.fadeIn().slideDown();
-    if(count>=10){
-      $("#"+removeCount).fadeOut();
-      $("#"+removeCount).remove();
-      removeCount++;
-    }
-    count++;
-    color++;
+      var txt = tweets.text;
+      var img = tweets.profile_image_url;
 
-      //if (txt.match(/value/i)){
-      //}
-  });
+      if(color%2===0) {
+        var style = "tweet0";
+      } else {
+        var style = "tweet1";
+      }
+      var tweet = $("<p id='"+count+"' class='"+style+"'><img src='"+img+"'/>"+txt+"</p>");
+      tweet.hide();
+      $("#tweets").prepend(tweet);
+      tweet.fadeIn().slideDown();
+      if(count>=10){
+        $("#"+removeCount).fadeOut();
+        $("#"+removeCount).remove();
+        removeCount++;
+      }
 
+      if(txt.match(/unca/i)){
+        termcount++;
+      };
+
+      count++;
+      color++;
+    });
   s.start();
+  });
 }
 
 $(document).ready( function() {
